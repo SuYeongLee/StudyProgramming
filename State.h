332379a9606eea28cac83d2340ca23cd83ef83f2
SuYeongLee <73162569+SuYeongLee@users.noreply.github.com>
2021-02-10 00:02:53 +0900

    State.h
    디자인패턴(스테이트패턴)

---
+++ State.h
@@ -0,0 +1,63 @@
+#pragma once
+
+#include <iostream>
+using namespace std;
+// 객체가 서로 참조하는경우는 다음과 같이 선언부와 정의부를 분리해야한다.
+class State;
+class StateOne;
+class StateTwo;
+class StateThree;
+
+class Context
+{
+    State* m_pState = NULL;
+public:
+    Context();
+    ~Context();
+
+    void Initialize(); // 
+    void Release(); // 
+    void SetState(State* state);
+    void GoNext();
+};
+
+
+class State
+{
+public:
+    State()
+    {
+        cout << typeid(this).name() << endl;
+    }
+    virtual ~State()
+    {
+        cout << "~" << typeid(this).name() << endl;
+    }
+    virtual void GoNext(Context* context) = 0;
+};
+
+class StateThree : public State
+{
+public:
+    StateThree();
+    ~StateThree();
+    void GoNext(Context* context) override;
+};
+
+class StateTwo : public State
+{
+public:
+    StateTwo();
+    ~StateTwo();
+    void GoNext(Context* context) override;
+};
+
+class StateOne : public State
+{
+public:
+    StateOne();
+    ~StateOne();
+    void GoNext(Context* context) override;
+};
+
+extern void StateTestMain();
